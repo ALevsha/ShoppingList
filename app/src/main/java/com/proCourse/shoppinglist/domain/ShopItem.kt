c53ff1dd75b1aa1cbb6ffbecd0264d66aaeaa63a
ALevsha <levsha26r@gmail.com>
2022-07-19 22:12:18 +0300

    lesson 3, Data слой

--- app/src/main/java/com/proCourse/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/proCourse/shoppinglist/domain/ShopItem.kt
@@ -1,8 +1,23 @@
 package com.proCourse.shoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
+    /**
+     * Обязательные (неизменяемые) поля указываются первыми
+     */
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    /**
+     * изменяемые после
+     */
+    var id: Int = UNDEFINED_ID // магическое число - плохая практика. Значит, что настоящий id не установлен,
+                     // и надо его установить
+){
+    /**
+     * для обозначения магических чисел используют константы, через companion object
+     */
+
+    companion object{
+        const val UNDEFINED_ID = -1
+    }
+}
