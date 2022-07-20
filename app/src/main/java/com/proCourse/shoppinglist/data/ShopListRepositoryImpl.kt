96f2d0fb8b03a283e23e6d3d6288788603cea82b
ALevsha <levsha26r@gmail.com>
2022-07-20 18:36:26 +0300

    lesson 7, добавление списков, их сортировка

--- app/src/main/java/com/proCourse/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/proCourse/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -14,7 +14,9 @@ import java.lang.RuntimeException
 object ShopListRepositoryImpl: ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id)})
+            // создание сортированного списка.
+            // В лямбде передается параметр, по которому идет сортировка
 
     private var autoIncrementId = 0
 
@@ -23,7 +25,7 @@ object ShopListRepositoryImpl: ShopListRepository {
      * добавлены элементы, или вызывались любые методы
      */
     init {
-       for (i in 0 until 10){
+       for (i in 0 until 10000){
            val item = ShopItem("Name $i", i, true)
            addShopItem(item)
        }
