bd0b10687973bc217b1cb6d6edd82008e1ad15a0
ALevsha <levsha26r@gmail.com>
2022-08-03 17:45:06 +0300

    доработка viewModel. Продвинутое использование LiveData

--- app/src/main/java/com/proCourse/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/proCourse/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,42 +1,14 @@
 package com.proCourse.shoppinglist.presentation
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import com.proCourse.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
-
-    private lateinit var editTextName: EditText
-    private lateinit var editTextCount: EditText
-    private var oldId = ""
-    private var oldEndabled = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-        editTextName = findViewById(R.id.editTextName)
-        editTextCount = findViewById(R.id.editTextCount)
-        val oldName = intent.getStringExtra("name")
-        val oldCount = intent.getStringExtra("count")
-        oldId = intent.getStringExtra("id").toString()
-        oldEndabled = intent.getStringExtra("enabled").toString()
-
-        editTextName.setText(oldName)
-        editTextCount.setText(oldCount)
-    }
-
-    override fun onBackPressed() {
-        val backIntent = Intent()
-        val newName = editTextName.text.toString()
-        val newCount = editTextCount.text.toString()
-        backIntent.putExtra("name", newName)
-        backIntent.putExtra("count", newCount)
-        backIntent.putExtra("id", oldId)
-        backIntent.putExtra("enabled", oldEndabled)
-        setResult(RESULT_OK, backIntent)
-        finish()
     }
 }
